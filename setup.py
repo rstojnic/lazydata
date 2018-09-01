41896cf918887a49ffdb5cddd06a8a4136b00ce0
Robert Stojnic <robert.stojnic@gmail.com>
2018-09-01 19:25:18 +0100

    First completely functional release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lazydata",
-    version="1.0.3",
+    version="1.0.4",
     author="Robert Stojnic",
     author_email="robert.stojnic@gmail.com",
     description="Scalable data dependencies",
